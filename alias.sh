d7d27c8641eb3eac15b757b35dcf24d2d2f59209
valentina <valentina.arenas@xertica.com>
2026-02-27 20:27:45 +0000

    se agregan alias para git

---
+++ alias.sh
@@ -0,0 +1,18 @@
+# Inicializaciones
+alias giti="git init --initial-branch=main"
+alias gitr="git remote add"
+alias gitro="git remote add origin"
+alias gitpsu='git push --set-upstream origin $(git branch --show-current)'
+
+# Desarrollo
+alias gits="git status"
+alias gita="git add"
+alias gitc="git commit -m"
+alias gitpl="git pull"
+alias gitps="git push"
+alias gitp="git pull && git push"
+
+# Manejo de ramas
+alias gitb="git branch"
+alias gitch="git checkout"
+alias gitm="git merge"
